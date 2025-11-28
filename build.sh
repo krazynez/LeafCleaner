ea0e2dee17d8db783bba47c87dc0b8a6fd2841e4
krazynez <brennenmurph@pm.me>
2025-11-28 08:33:18 -0500

    Added readme, and updated script

--- build.sh
+++ build.sh
@@ -2,3 +2,11 @@
 
 make clean && make
 python ./bin/psptools/pack_ms_game.py --vanity 'Leaf Cleaner' EBOOT.PBP EBOOT.PBP
+
+if [ -d "PSP" ]; then
+	rm -rf PSP
+fi
+
+mkdir -p PSP/GAME/LeafCleaner
+
+mv EBOOT.PBP PSP/GAME/LeafCleaner/
